81ec9d2c365d62d33f4925acc68fdd8c1211b7b6
kemal <kemal@example.com>
2025-10-22 15:46:42 +0200

    wip: claude code is organising web to follow planet eslint config, pre claudecode resolving all issues

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,14 +1,40 @@
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
 import planetConfig from 'eslint-config-planet/react.js'
 
 export default [
   ...planetConfig,
   {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
     rules: {
       // React 19 / Next.js doesn't require React import
       'react/react-in-jsx-scope': 'off',
       'react/jsx-uses-react': 'off',
       // TypeScript handles prop types
       'react/prop-types': 'off',
+      // Use TypeScript's unused vars rule instead
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
 ]
